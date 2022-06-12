d797aa90f4f7492fe034c4333721238fb420de9c
binaryleo <leomouracss@fastmail.com>
2022-06-12 14:20:49 -0300

    chore: add splashScren

--- App.tsx
+++ App.tsx
@@ -1,32 +1,42 @@
 import 'react-native-gesture-handler';
-
-import React from 'react';
+import React, {useState,useEffect,useCallback} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import AppLoading from 'expo-app-loading';
-import {
-  useFonts,
-  Inter_400Regular,
-  Inter_500Medium,
-  Inter_700Bold
-} from '@expo-google-fonts/inter';
-
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import {Inter_400Regular, Inter_500Medium, Inter_700Bold, } from '@expo-google-fonts/inter'
 import { Routes } from './src/routes';
 import { View } from 'react-native';
 import { theme } from './src/styles/theme';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Inter_400Regular,
-    Inter_500Medium,
-    Inter_700Bold,
-  });
+  const [appIsReady, setAppIsReady] = useState(false);
+  useEffect(() => {
+    (async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({Inter_400Regular, Inter_500Medium, Inter_700Bold});
+      }
+      catch {
+      console.log("error loading fonts");
+      }
+      finally {
+        setAppIsReady(true);
+      }
+    })();
+  }, []);
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
+  const onLayout = useCallback(() => {
+    if (appIsReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
-
+  
   return (
-    <View
+    <View onLayout={onLayout}
       style={{
         flex: 1,
         backgroundColor: theme.colors.background
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import { View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React from "react";
+import { View } from "react-native";
+import * as AuthSession from "expo-auth-session";
+import { useNavigation } from "@react-navigation/native";
+import { Button } from "../../components/Button";
+import { SignInContent } from "../../components/SignInContent";
 
-import { Button } from '../../components/Button';
-import { SignInContent } from '../../components/SignInContent';
+import { styles } from "./styles";
 
-import { styles } from './styles';
-
-type AuthResponse = {
+/* type AuthResponse = {
   type: string;
   params: {
     access_token: string;
-  }
-}
+  }D
+} */
 
 export function SignIn() {
   const navigation = useNavigation();
-
+  const credentialsConfig = {
+    CLIENT_ID:
+      "941596417298-cssccqat0b4gchh532eo8e7b64prnmjf.apps.googleusercontent.com",
+    REDIRECT_URI: "https://auth.expo.io/@binaryleo/oauth2app",
+    RESPONSE_TYPE: "token",
+    SCOPE: encodeURI("profile email"),
+  };
   async function handleSignIn() {
-    navigation.navigate('Profile');
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${credentialsConfig.CLIENT_ID}&redirect_uri=${credentialsConfig.REDIRECT_URI}&response_type=${credentialsConfig.RESPONSE_TYPE}&scope=${credentialsConfig.SCOPE}`;
+    const response = await AuthSession.startAsync({ authUrl });
+    console.log(response);
+    navigation.navigate("Profile");
   }
 
   return (
